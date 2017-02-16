226301f728d06ff33f142c75cb28495910c920a1
Vinesh Kannan <vkannan3@hawk.iit.edu>
2017-02-16 10:52:24 -0600

    Create league file

---
+++ database/league.js
@@ -0,0 +1,13 @@
+var League = {
+
+	/*
+	 * Generate schedules for a league of users
+	 * Input: list of userids, must be unique and even-length
+	 * Output: some wild data structure
+	 */
+	generateSchedule: (users) => {
+		var schedule = false;
+		return schedule;
+	}
+
+}
