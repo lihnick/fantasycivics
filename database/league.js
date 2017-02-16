9c3332ae4a3aead55b5b84b63559ee657000d40f
Vinesh Kannan <vkannan3@hawk.iit.edu>
2017-02-16 12:02:00 -0600

    Implement naive schedule generator

--- database/league.js
+++ database/league.js
@@ -2,6 +2,8 @@ var League = {
 
 	PLAYERS_PER_ROSTER: 5,
 
+	DEFAULT_SEASON_LENGTH: 3,
+
 	/*
 	 * Fix list of users for a league
 	 * Input: list of userids, map of bot users
@@ -14,8 +16,8 @@ var League = {
 		}
 		else if(users.length % 2 !== 0){
 			var newUsers = Util.clone(users);
-			var bot = Util.getRandomKey(bots);
-			newUsers.push(bots);
+			var botid = Util.getRandomKey(bots);
+			newUsers.push(botid);
 			res = newUsers;
 		}
 		return res;
@@ -23,19 +25,45 @@ var League = {
 
 	/*
 	 * Generate schedules for a league of users
-	 * Input: list of userids (must be unique and even-length)
-	 * Output: some wild data structure
+	 * Input: list of userids (must be unique and even-length), number of weeks in season
+	 * Output: a list of lists containing the matches for the season
 	 */
-	generateSchedule: (users) => {
-		var schedule = false;
+	generateSchedule: (users, weeksInput) => {
+		var schedule = [];
+		var weeks = weeksInput || League.DEFAULT_SEASON_LENGTH;
 		if(users.length < 1){
 			throw new Error('List of userids is empty.');
 		}
 		else if(users.length % 2 !== 0){
 			throw new Error('List of userids must be even in length.');
 		}
+		else if(weeks < 1){
+			throw new Error('Season must be at least one week long');
+		}
 		else{
-
+			var allWeeks = [];
+			var half = Math.floor(users.length / 2);
+			for(var a = 1; a < users.length; a++){
+				var thisWeek = [];
+				for(var u = 0; u < half; u++){
+					var nidx = (u + a) % users.length;
+					var opponent = users[nidx];
+					if(users[u] === opponent){
+						throw new Error('A user cannot be matched up against their own team.');
+					}
+					thisWeek.push({
+						home: users[u],
+						away: opponent
+					});
+				}
+				allWeeks.push(thisWeek);
+			}
+			for(var w = 0; w < weeks; w++){
+				// Later: Prevent repeated matchups in short leagues
+				// Later: Equalize number of home/away matches
+				var weekMatches = Util.getRandomItem(allWeeks);
+				schedule.push(weekMatches);
+			}
 		}
 		return schedule;
 	},
