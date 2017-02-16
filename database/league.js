634cd9e476217cb35f96cc27fd7c2904eb99caeb
Vinesh Kannan <vkannan3@hawk.iit.edu>
2017-02-16 10:54:08 -0600

    Document generate rosters function

--- database/league.js
+++ database/league.js
@@ -2,12 +2,22 @@ var League = {
 
 	/*
 	 * Generate schedules for a league of users
-	 * Input: list of userids, must be unique and even-length
+	 * Input: list of userids (must be unique and even-length)
 	 * Output: some wild data structure
 	 */
 	generateSchedule: (users) => {
 		var schedule = false;
 		return schedule;
+	},
+	
+	/*
+	 * Generate rosters for a league of users from given players
+	 * Input: list of userids (must be unique and even-length), map of players
+	 * Output: map of rosters to userids
+	 */
+	generateRosters: (users) => {
+		var rosterMap = {};
+		return rosterMap;
 	}
 
 }
