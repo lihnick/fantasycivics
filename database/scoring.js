d2dbdffeeafefb1c57e0b53c57c15cca43cfbc59
Vinesh Kannan <vkannan3@hawk.iit.edu>
2017-02-16 14:45:56 -0600

    Query Socrata datasets with data ranges

--- database/scoring.js
+++ database/scoring.js
@@ -1,35 +1,42 @@
-var SOCRATA_URL = "https://data.cityofchicago.org/resource/787j-mys9.json"
+var SOCRATA_URL = "https://data.cityofchicago.org/resource/";
 var SECRET_TOKEN = "Le00VXF0GK0d8D1tTn2v6Vkpl";
 
-
-function getData(url, query, callback, limit){
-	query['$$app_token'] = SECRET_TOKEN;
-	query['$limit'] = limit || 10;
-	$.ajax({
-		url: url,
-		method: "GET",
-		dataType: "json",
-		data: query,
-		success: function(data, status, jqxhr){
-			callback(data);
-		},
-		error: function(jqxhr, status, error){
-			console.error("Critical Error!");
-		}
-	});
+var DATASETS = {
+	pot_holes: '787j-mys9.json'
 }
-
-var LIMIT = 3;
-
-getData(SOCRATA_URL, {}, function(data){
-	main(data);
-}, LIMIT);
-
-function main(data){
-	console.log(data);
-}
-
 
 var Scoring = {
+
+	getSocrataData: (url, query, callback, limit) => {
+		query['$$app_token'] = SECRET_TOKEN;
+		// Deactivated query limits // query['$limit'] = limit;
+		$.ajax({
+			url: url,
+			method: "GET",
+			dataType: "json",
+			data: query,
+			success: function(data, status, jqxhr){
+				callback(data);
+			},
+			error: function(jqxhr, status, error){
+				throw new Error("Socrata request failed: " + error);
+			}
+		});
+	},
+
+	buildDateQuery: (field, from, to) => {
+		return field + " between '" + new Date(from).toISOString().split('.')[0] + "' and '" + new Date(to).toISOString().split('.')[0] + "'";
+	},
+
+	queryDataset: (dataset, query) => {
+		return new Promise((resolve, reject) => {
+			try{
+				Scoring.getSocrataData(SOCRATA_URL + DATASETS[dataset], query, resolve);
+			}
+			catch(e){
+				reject(e);
+			}
+		});
+	}
 
 }
