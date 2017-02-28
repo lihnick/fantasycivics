fc4dc2c86c23cd28b19ce398d93730bad6bfa853
Vinesh Kannan <vkannan3@hawk.iit.edu>
2017-02-28 14:10:01 -0600

    Render current leagues

--- app/join.js
+++ app/join.js
@@ -2,29 +2,55 @@ var Database = InitDatabase();
 var USER = false;
 
 Database.Auth.getCurrentUser().then((user) => {
-	USER = user;
+	login(user);
 }).catch((err) => {
 	if(err === 'No user currently authenticated.'){
-		Database.Auth.signInUser().then((user) => {
-			USER = user;
-		}).catch(console.error);
+		console.log(err);
 	}
 	else{
 		console.error(err);
 	}
 });
 
+var loginBtn = document.getElementById('login-button')
+loginBtn.addEventListener('click', (e) => {
+	Database.Auth.signInUser().then((user) => {
+		login(user);
+	}).catch(console.error);
+});
+
+function main(){
+	console.log('Logged in as: ', USER);
+
+	var page = document.getElementById('page');
+		page.style.display = 'block';
+	var login = document.getElementById('login');
+		login.style.display = 'none';
+	var createBtn = document.getElementById('create-button')
+	
+	createBtn.addEventListener('click', (e) => {
+		createLeagueInvitation();
+	});
+	
+	renderUserLeagues();
+
+}
+
+function login(user){
+	USER = user;
+	Database.updateUser(user).then((done) => {
+		main();
+	}).catch(console.error);
+}
+
 function createLeagueInvitation(){
 	Database.createLeagueInvitation({
 		userid: USER.userid,
 		name: 'ChiHackNight Demo League'
 	}).then((res) => {
-		presentInvitation(res.inviteid);
+		console.log('Join League with Code: ' + res.inviteid);
+		renderUserLeagues();
 	}).catch(console.error);
-}
-
-function presentInvitation(inviteid){
-
 }
 
 function joinLeague(inviteid){
@@ -40,11 +66,33 @@ function presentJoined(){
 
 }
 
-function showLeagueMembers(inviteid){
+function renderUserLeagues(){
 	Database.getLeagueInvitations({
 		userid: USER.userid
 	}).then((res) => {
-		var leagueStub = res[inviteid];
+		var output = document.getElementById('waiting-leagues');
+		var html = '';
+		if(res){
+			for(var inviteid in res){
+				var stub = res[inviteid];
+				var lh = '';
+					lh += '<h3>' + stub.league.name + '</h3>'
+					lh += '<h4>Invite URL</h4>'
+					lh += '<p>' + window.location.href + '?code=' + inviteid + '</p>'
+					lh += '<h4>Members</h4>'
+					lh += '<ul>'
+					for(var uid in stub.members){
+						lh += '<li>' + uid + '</li>'
+					}
+					lh += '</ul>'
+					lh += '<button>Begin League</button>'
+				html += lh
+			}
+		}
+		else{
+			html += '<p>No leagues: start one!</p>'
+		}
+		output.innerHTML = html;
 	}).catch(console.error);
 }
 
