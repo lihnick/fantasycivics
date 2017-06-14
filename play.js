f5d4751bed8551b0b44f3ea1437874d41070761b
Vinesh Kannan <vinesh@omnipointment.com>
2017-06-14 15:50:18 -0500

    Add custom roster view and events

--- play.js
+++ play.js
@@ -8,69 +8,17 @@ let config = {
 let DatabaseFirebase = firebase.initializeApp(config, 'Fantasy Civics Database');
 let DatabaseInstance = DatabaseFirebase.database();
 
-let displayScoredRoster = (id, module, system) => {
-	let out = document.getElementById(id);
-	out.innerHTML = '';
-	let scores = module.scoreByFunction(system);
-	let columns = [
-		{
-			header: 'Name',
-			cell: (player) => {
-				return player.name;
-			}
-		},
-		{
-			header: 'Ward',
-			cell: (player) => {
-				return player.ward;
-			}
-		},
-		{
-			header: 'Total',
-			cell: (player) => {
-				return player.score;
-			}
-		}
-	];
-	for(let bid in system.breakdown){
-		let cat = system.breakdown[bid];
-		columns.push({
-			header: cat.header || 'N/A',
-			cell: (player) => {
-				/*let div = document.createElement('div');
-					div.innerText = player.breakdown[bid].score;
-					div.addEventListener('click', e => {
-						vex.dialog.alert(cat.text);
-					});
-				return div;*/
-				return player.breakdown[bid].score;
-			}
-		});
-	}
-	columns.push({
-		header: 'Breakdown',
-		cell: (player) => {
-			let button = document.createElement('button');
-				button.innerText = 'View';
-				button.addEventListener('click', e => {
-					console.log(player);
-					system.clickRow(player, module);
-				});
-			return button;
-		}
-	});
-	let table = RosterView(scores, {
-		module: module,
-		columns: columns
-	});
-	out.appendChild(table);
-	Sortable.initTable(table);
-}
+let allPlayers = {};
+Object.keys(PLAYER_MAP).forEach(pid => {
+	let player = PLAYER_MAP[pid];
+		player.pid = pid;
+	allPlayers[pid] = player;
+});
 
 let sm = ScoringModule({
 	database: DatabaseInstance,
 	datasets: SOCRATA_DATASETS,
-	players: PLAYER_MAP,
+	players: allPlayers,
 	range: {
 		from: new Date('4/1/2017').getTime(),
 		to: new Date('4/30/2017').getTime()
@@ -78,30 +26,51 @@ let sm = ScoringModule({
 });
 sm.init().then(playerMap => {
 
-	//console.log(playerMap);
-
 	window.playerMap = playerMap;
-
 	var loadingScreen = document.getElementById('loading');
-	loadingScreen.style.display = 'none';
+		loadingScreen.style.display = 'none';
 
-	displayScoredRoster('whole-league', sm, MAIN_SCORING_SYSTEM);
+	let leagueView = CustomRosterView('whole-league', {
+		module: sm,
+		system: MAIN_SCORING_SYSTEM,
+		columns: ['name', 'ward', 'total', 'breakdown', 'add']
+	});
 
 	let rosterMap = {};
 	let count = 0;
 	for(let pid in playerMap){
-		if(count < 5){
+		if(count < 3){
 			rosterMap[pid] = playerMap[pid];
 			count++;
 		}
 	}
 
-	console.log(rosterMap);
+	let rosterView = CustomRosterView('your-roster', {
+		module: getChildModule(sm, rosterMap),
+		system: MAIN_SCORING_SYSTEM,
+		columns: ['name', 'ward', 'total', 'drop']
+	});
 
-	let cm = getChildModule(sm, rosterMap);
-	displayScoredRoster('your-roster', cm, MAIN_SCORING_SYSTEM);
+	leagueView.on('add', (player) => {
+		let size = Object.keys(rosterView.module.players).length + 1;
+		if(size > 5){
+			vex.dialog.alert('Too many players! Drop someone from your roster before adding.');
+		}
+		else{
+			rosterView.module.players[player.pid] = player;
+			rosterView.render();
+		}
+	});
 
-}).catch(console.error);
+	rosterView.on('drop', (player) => {
+		delete rosterView.module.players[player.pid];
+		rosterView.render();
+	});
+
+}).catch((err) => {
+	console.trace();
+	console.error(err);
+});
 
 
 
